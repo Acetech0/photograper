782cbd18f7530d91cd4582384b0725f6ee85a773
Acetech0 <acem140804@gmail.com>
2026-04-21 23:23:53 +0530

    zero

--- shmaik/app/components/Header.js
+++ shmaik/app/components/Header.js
@@ -24,6 +24,7 @@ export default function Header({ activePage, onNavigate }) {
           >
             About
           </a>
+          
           <a
             href="#"
             id="nav-contact"
