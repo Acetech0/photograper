fb4370a2a4a39a21fe09dfc9f131a7476e957d17
Acetech0 <acem140804@gmail.com>
2026-05-09 19:07:55 +0530

    privacy

--- shmaik/app/components/Footer.js
+++ shmaik/app/components/Footer.js
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer>
       <div className="footer-left">
-        <a href="#">Privacy Policy</a>
-        <a href="#">Accessibility Statement</a>
+        {/* <a href="#">Privacy Policy</a>
+        <a href="#">Accessibility Statement</a> */}
       </div>
       <div className="footer-center">
         <a href="https://www.behance.net/shamikdeshmukh" target="_blank" rel="noopener noreferrer">
